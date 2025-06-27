3d5e92c5efdc77d4700e0d87b440c8e480cda287
motconkien <hynuiux@gmail.com>
2025-06-27 16:14:30 +0900

    updated

--- CoffeeShop/settings.py
+++ CoffeeShop/settings.py
@@ -15,7 +15,8 @@ import os
 import dj_database_url
 
 #for deploy
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['coffeeshop-knmn.onrender.com', 'www.coffeeshop-knmn.onrender.com']
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
